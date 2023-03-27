234bc1ec76073cd5343b914da6b65cfe950dd0ab
saferboy <muxtordev@gmail.com>
2023-03-27 13:43:29 +0500

    html struktura hbs ga o'zgardi

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
+import express from "express"
+import { engine } from "express-handlebars"
+
+const app = express()
+
 import dotenv from "dotenv"
 dotenv.config()
 
-import express from "express"
+
+app.engine('.hbs', engine({ extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './pages')
+
+
 import cors from "cors"
 
-import api from "./api/router"
+// import api from "./api/router"
 
-const app = express()
+app.use('/', express.static('./public'))
 
 app.use(cors())
 app.use(express.json())
@@ -15,7 +25,30 @@ app.use(express.urlencoded({extended: true}))
 
 const port = process.env.PORT
 
-app.use('/api', api)
+// app.use('/api', api)
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/send', (req, res) => {
+    res.render('sendLink')
+})
+
+app.get('/setting', (req, res) => {
+    res.render('settings')
+})
+
+app.get('/sms', (req, res) => {
+    res.render('sms-codes')
+})
+
+app.get('/succes', (req, res) => {
+    res.render('success-error')
+})
+
+
 
 app.listen(port, () => {
     console.log(`Server is running ${port}`)
