781110379b66772d588b2e461de3fe40e6d09308
saferboy <muxtordev@gmail.com>
2023-03-29 15:11:04 +0500

    prisma settings

---
+++ prisma/migrations/20230329100811_uzmega/migration.sql
@@ -0,0 +1,16 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('Success', 'Error');
+
+-- CreateTable
+CREATE TABLE "Payment" (
+    "id" SERIAL NOT NULL,
+    "client" TEXT NOT NULL,
+    "request_date" TIMESTAMP(3) NOT NULL,
+    "paid_date" TIMESTAMP(3) NOT NULL,
+    "amount" TEXT NOT NULL,
+    "trafik" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "status" "Status" NOT NULL,
+
+    CONSTRAINT "Payment_pkey" PRIMARY KEY ("id")
+);
